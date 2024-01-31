6e03f8f5bf8f0bf7013fd5c179f28f50f84be573
ReQezeR <44375216+ReQezeR@users.noreply.github.com>
2024-01-31 22:22:23 +0100

    feat: modify client read timeout

--- app/src/main/java/pl/reqevo/starwarsapp/network/RetrofitClient.kt
+++ app/src/main/java/pl/reqevo/starwarsapp/network/RetrofitClient.kt
@@ -1,7 +1,9 @@
 package pl.reqevo.starwarsapp.network
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
@@ -9,7 +11,13 @@ object RetrofitClient {
     private const val BASE_URL = "https://swapi.dev/api/"
 
     fun getInstance(): Retrofit {
-        return Retrofit.Builder().baseUrl(BASE_URL)
+        val client = OkHttpClient.Builder()
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(JacksonConverterFactory.create())
             .build()
     }
