8a601bedffd8d9ba3b42bb07ead33974b76b66c1
ReQezeR <44375216+ReQezeR@users.noreply.github.com>
2024-01-31 22:22:43 +0100

    refactor: update person api

--- app/src/main/java/pl/reqevo/starwarsapp/network/PersonApi.kt
+++ app/src/main/java/pl/reqevo/starwarsapp/network/PersonApi.kt
@@ -1,10 +1,16 @@
 package pl.reqevo.starwarsapp.network
 
+import pl.reqevo.starwarsapp.models.Person
 import pl.reqevo.starwarsapp.models.PersonResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface PersonApi {
     @GET("people")
-    fun getPeople(): Call<PersonResponse>
+    fun getPeople(@Query("page") page: Int): Call<PersonResponse>
+
+    @GET("people/{id}/")
+    fun getPerson(@Path("id") id: Int): Call<Person>
 }
